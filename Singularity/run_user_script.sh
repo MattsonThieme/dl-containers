ead1301658bcbf419d10a7f37837d701d1f98ce4
mattsonthieme <mattson.thieme@gmail.com>
2018-08-27 12:46:32 -0400

    Organize variables; remove duplicate lines

--- Singularity/run_user_script.sh
+++ Singularity/run_user_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# To run: bash run_user_script.sh <hostfile> <workers per node> <inter op threads>
+# Note: The total number of workers deployed will be the number of workers per node * number of nodes
 
 if [ "$1" == "-h" ]; then
   echo "Usage: `basename $0` [<logdir>] [<hostfile>] [<workers per node>] [<inter op threads>]"
@@ -10,38 +12,37 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 
-PATH_TO_SINGULARITY="/root/singularity/bin/singularity"
-PATH_TO_SIMG="/root/tf-hvd.simg"
-SING_EXEC_CMD="sudo ${PATH_TO_SINGULARITY} exec ${PATH_TO_SIMG}" 
+# Optional arguments (see help above)
 
-# Optional arguments
 logdir=${1:-_multiworker}     # Default suffix is _multiworker
 node_ips=${2:-hosts.txt}      # Default is the hosts.txt file
-export num_workers_per_node=${3:-2}  # Default 2 workers per node
-export num_inter_threads=${4:-2} # Default to 2 inter_op threads
+num_workers_per_node=${3:-2}  # Default 2 workers per node
+num_inter_threads=${4:-2}     # Default to 2 inter_op threads
 
+# Update the following variables to reflect your configuration
 # The workspace directory should contain both data and code
+
 PATH_TO_WORKSPACE="/root/"
 PATH_TO_SCRIPT="/root/topologies/distributed_unet/Horovod/hvd_train.py"
 PATH_TO_DATA="/root/data"
-OMP_NUM_THREADS=22
-HOSTNAMES=$( cat ${node_ips} )
+PATH_TO_SINGULARITY="/root/singularity/bin/singularity"
+PATH_TO_SIMG="/root/tf-hvd.simg"
+
+# Calculate MPI execution parameters given workers/node, number of nodes, and core counts on each node
 
 export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` # Total number of physical cores per socket
-export num_nodes=`awk -F, '{print NF}' ${node_ips} | head -1 ` # Hosts.txt should contain IP addresses separated by commas
+export num_nodes=`awk -F, '{print NF}' ${node_ips} | head -1 `
+export hostnames=$( cat ${node_ips} )
 export num_sockets=`lscpu | grep "Socket(s)" | cut -d':' -f2 | sed "s/ //g"`   # Number of sockets per node
-
 export num_processes=$(( $num_nodes * $num_workers_per_node )) # Total number of workers across all nodes
-export ppr=$(( $num_workers_per_node / $num_sockets ))
-export pe=$(( $physical_cores / $ppr ))
-
-export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` # Total number of physical cores per socket
-export num_threads=$(( ${ppr} * $physical_cores )) # Total number of physical cores on this machine
+export ppr=$(( $num_workers_per_node / $num_sockets ))  # Processes per resource
+export pe=$(( $physical_cores / $ppr ))  # Cores per process
+export num_threads=$(( ${ppr} * $physical_cores ))  # Total number of physical cores on this machine
 
 # Execute script
 echo "Running ${num_processes} processes across ${num_nodes} node(s)..."
 
-mpirun --allow-run-as-root -np ${num_processes} --map-by ppr:${ppr}:socket:pe=${OMP_NUM_THREADS} -H ${HOSTNAMES} \
+mpirun --allow-run-as-root -np ${num_processes} --map-by ppr:${ppr}:socket:pe=${pe} -H ${hostnames} \
 --report-bindings --oversubscribe -x LD_LIBRARY_PATH -x OMP_NUM_THREADS=${OMP_NUM_THREADS} sudo ${PATH_TO_SINGULARITY} \
 exec -B ${PATH_TO_WORKSPACE} ${PATH_TO_SIMG} python ${PATH_TO_SCRIPT} --datadir=${PATH_TO_DATA} \
 --logdir="tensorboard${1}" --num_inter_threads=${num_inter_threads} --num_threads=$num_threads
