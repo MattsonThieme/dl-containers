3e27f26dd9c1be17d64a8281cfc49dec990b1cee
mattsonthieme <mattson.thieme@gmail.com>
2018-09-27 15:05:55 -0700

    Add run script for openvino demo

---
+++ Singularity/run_openvino_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# To run: sudo singularity exec -B /home/,/usr/ openvino.simg bash run_openvino_demo.sh 
+
+cd /home/bduser/intel/computer_vision_sdk_2018.3.343/deployment_tools/demo/
+bash demo_squeezenet_download_convert_run.sh
